9ae4bb3f6af05a4b98e07b91708bbaf76a36681a
Cherry Lam <siuding0601@gmail.com>
2017-06-07 14:37:28 -0700

    Finished tower of hanoi game and tested with spec

--- lib/toh.rb
+++ lib/toh.rb
@@ -1,3 +1,27 @@
 class TOH
 
+  attr_accessor :tower
+
+  def initialize
+    @tower = [[3,2,1],[],[]]
+  end
+
+  def move(from_pile, to_pile)
+    if @tower[from_pile].empty?
+      raise "Empty pile"
+    elsif @tower[to_pile].empty?
+      @tower[to_pile].push(@tower[from_pile].pop)
+    elsif @tower[to_pile][-1] < @tower[from_pile][-1]
+      raise "Disc is too big"
+    else
+      @tower[to_pile].push(@tower[from_pile].pop)
+    end
+
+  end
+
+  def won?
+    @tower[1] == [3,2,1] || @tower[2] == [3,2,1]
+  end
+
+
 end
--- spec/toh_spec.rb
+++ spec/toh_spec.rb
@@ -12,33 +12,36 @@ describe TOH do
   end
 
   describe "#move" do
-    it "moves the disc from 'from_pile' tower to 'to_pile' tower and lands on top" do
-      game.move(0, 1)
-      expect(game.tower[1][-1]).to eq(game.tower[0][-1])
+
+    context "when it is a valid move" do
+      it "moves the disc from 'from_pile' tower to 'to_pile' tower and lands on top" do
+        game.tower = [[3,2],[],[1]]
+        game.move(0, 1)
+        expect(game.tower[1][-1]).to eq(2)
+      end
     end
 
-    context 'when the pile that is being drawn from is empty' do
-      before(:each) do
-        game.tower = [[3,2],[],[1]]
+    context "when the move is invalid" do
+    let(:game1) do
+        game1 = TOH.new
+        game1.tower = [[3,2],[],[1]]
+        game1
+      end
+      context 'when the pile that is being drawn from is empty' do
+        it "should raise error that indicates the pile is empty" do
+          expect { game1.move(1, 2)}.to raise_error("Empty pile")
+        end
       end
 
-      it "should raise error that indicates the pile is empty" do
-        expect { game.move(1, 2)}.to raise_error("Empty pile")
+    context "when the disc being moved is bigger than the last disc on the to_pile" do
+
+      it "should raise error that indicates the disc is too big to be moved" do
+        p game.tower
+        expect {game1.move(0, 2)}.to raise_error("Disc is too big")
       end
     end
 
   end
-
-    context "when the disc being moved is bigger than the last disc on the to_pile" do
-      before(:each) do
-        game.tower = [[3,2],[],[1]]
-      end
-
-      it "should raise error that indicates the disc is too big to be moved" do
-        expect { game.move(0, 2) }.to raise_error("Disc is too big")
-      end
-
-    end
 
     describe "#won?" do
 
@@ -47,7 +50,7 @@ describe TOH do
           game.tower = [[],[3,2,1],[]]
         end
 
-        it "should end the game and display a message" do
+        it "should end the game and return true" do
           expect(game.won?).to eq true
         end
 
@@ -65,7 +68,5 @@ describe TOH do
       end
 
     end
-
-
-
+  end
 end
